5e9439037cb0734782ceb31bb3314183e48db230
iGabyTM <contactgabytm@gmail.com>
2021-08-12 09:46:37 +0300

    Begin working on vouchers

---
+++ src/main/kotlin/me/gabytm/minecraft/arcanevouchers/vouchers/VoucherManager.kt
@@ -0,0 +1,7 @@
+package me.gabytm.minecraft.arcanevouchers.vouchers
+
+class VoucherManager {
+
+    private val loadedVouchers = mutableMapOf<String, Voucher>()
+
+}
---
+++ src/main/kotlin/me/gabytm/minecraft/arcanevouchers/vouchers/VoucherSettings.kt
@@ -0,0 +1,15 @@
+package me.gabytm.minecraft.arcanevouchers.vouchers
+
+class VoucherSettings(
+    val hasConfirmation: Boolean
+) {
+
+    companion object {
+
+        fun from(): VoucherSettings {
+            TODO()
+        }
+
+    }
+
+}
