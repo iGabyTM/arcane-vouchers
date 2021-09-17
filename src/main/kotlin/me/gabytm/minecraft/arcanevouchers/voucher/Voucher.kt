84454a3759b628ce089e8528d6ba5956e62bfd65
iGabyTM <contactgabytm@gmail.com>
2021-09-17 15:35:21 +0300

    Finish the implementation for vouchers creation (`Voucher#from()`)

--- src/main/kotlin/me/gabytm/minecraft/arcanevouchers/voucher/Voucher.kt
+++ src/main/kotlin/me/gabytm/minecraft/arcanevouchers/voucher/Voucher.kt
@@ -1,12 +1,15 @@
 package me.gabytm.minecraft.arcanevouchers.voucher
 
+import de.tr7zw.nbtapi.NBTItem
+import me.gabytm.minecraft.arcanevouchers.Constant.Nbt
 import me.gabytm.minecraft.arcanevouchers.actions.ArcaneAction
 import me.gabytm.minecraft.arcanevouchers.actions.ArcaneActionManager
+import me.gabytm.minecraft.arcanevouchers.items.ItemCreator
 import org.bukkit.Material
 import org.bukkit.configuration.ConfigurationSection
 import org.bukkit.inventory.ItemStack
 
-class Voucher(
+class Voucher private constructor(
     val settings: VoucherSettings,
     val item: ItemStack,
     val actions: List<ArcaneAction>
@@ -14,13 +17,19 @@ class Voucher(
 
     companion object {
 
-        const val NBT = "ArcaneVouchers"
-
-        // TODO: 14-Aug-21 create the item based on config options 
-        fun from(config: ConfigurationSection, actionManager: ArcaneActionManager): Voucher {
+        fun from(config: ConfigurationSection, actionManager: ArcaneActionManager, itemCreator: ItemCreator): Voucher {
             val settings = VoucherSettings.from(config.getConfigurationSection("settings"))
+            val item = itemCreator.create(true, config.getConfigurationSection("item"), Material.PAPER).apply {
+                val nbtItem = NBTItem(this)
+                val compound = nbtItem.getCompound(Nbt.VOUCHER_COMPOUND)
+
+                compound.addCompound(Nbt.ARGUMENTS_COMPOUND)
+                compound.setString(Nbt.VOUCHER_NAME, config.name)
+                nbtItem.item
+            }
             val actions = actionManager.parseActions(config.getStringList("actions"))
-            return Voucher(settings, ItemStack(Material.STONE), actions)
+
+            return Voucher(settings, item, actions)
         }
 
     }
