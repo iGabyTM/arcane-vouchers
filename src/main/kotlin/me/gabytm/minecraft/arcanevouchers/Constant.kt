64a346dc05c24cc7be43575893d4c72f45fa641e
iGabyTM <contactgabytm@gmail.com>
2021-09-17 15:31:35 +0300

    Add constants for NBT tags

--- src/main/kotlin/me/gabytm/minecraft/arcanevouchers/Constant.kt
+++ src/main/kotlin/me/gabytm/minecraft/arcanevouchers/Constant.kt
@@ -1,10 +1,25 @@
 package me.gabytm.minecraft.arcanevouchers
 
+import com.google.gson.GsonBuilder
 import net.kyori.adventure.text.minimessage.MiniMessage
 
 object Constant {
 
+    val GSON = GsonBuilder().create()
     val MINI = MiniMessage.markdown()
     val SEPARATOR = Regex(";")
+
+    object Nbt {
+
+        // The main compound
+        const val VOUCHER_COMPOUND = "ArcaneVouchers"
+        // Arguments compound
+        const val ARGUMENTS_COMPOUND = "Arguments"
+        // Receiver's name
+        const val RECEIVER_NAME = "Receiver"
+        // Voucher's name
+        const val VOUCHER_NAME = "Name"
+
+    }
 
 }
