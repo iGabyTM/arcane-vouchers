7705d72327b4e4e89926d38a9f417e0e4b7c432e
iGabyTM <contactgabytm@gmail.com>
2021-11-24 15:11:26 +0200

    remove redundant constructor

--- src/main/kotlin/me/gabytm/minecraft/arcanevouchers/actions/ArcaneContext.kt
+++ src/main/kotlin/me/gabytm/minecraft/arcanevouchers/actions/ArcaneContext.kt
@@ -3,7 +3,4 @@ package me.gabytm.minecraft.arcanevouchers.actions
 import me.gabytm.util.actions.actions.Context
 import org.bukkit.entity.Player
 
-class ArcaneContext(val actions: List<ArcaneAction>, data: MutableMap<String, Any>) :
-    Context<Player>(mutableListOf(), data) {
-
-}
+class ArcaneContext(val actions: List<ArcaneAction>, data: MutableMap<String, Any>) : Context<Player>(mutableListOf(), data)
