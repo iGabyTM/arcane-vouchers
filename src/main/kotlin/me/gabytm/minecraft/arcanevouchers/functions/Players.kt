ca492a0b90d7c081dd1d56ed3406cb583c16965f
iGabyTM <contactgabytm@gmail.com>
2021-12-06 23:03:44 +0200

    add function to get player's item in hand

---
+++ src/main/kotlin/me/gabytm/minecraft/arcanevouchers/functions/Players.kt
@@ -0,0 +1,14 @@
+package me.gabytm.minecraft.arcanevouchers.functions
+
+import me.gabytm.minecraft.arcanevouchers.ServerVersion
+import org.bukkit.entity.Player
+import org.bukkit.inventory.ItemStack
+
+@Suppress("DEPRECATION")
+fun Player.item(): ItemStack {
+    return if (ServerVersion.HAS_OFF_HAND) {
+        return this.inventory.itemInMainHand
+    } else {
+        this.inventory.itemInHand
+    }
+}
