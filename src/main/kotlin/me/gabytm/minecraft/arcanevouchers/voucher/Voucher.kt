0b776c8070c54dc29c0522da5865d82a39befec2
iGabyTM <contactgabytm@gmail.com>
2021-12-06 22:33:23 +0200

    fix issue with NBT tags not being set to the item

--- src/main/kotlin/me/gabytm/minecraft/arcanevouchers/voucher/Voucher.kt
+++ src/main/kotlin/me/gabytm/minecraft/arcanevouchers/voucher/Voucher.kt
@@ -7,14 +7,20 @@ import me.gabytm.minecraft.arcanevouchers.actions.ArcaneActionManager
 import me.gabytm.minecraft.arcanevouchers.items.ItemCreator
 import org.bukkit.Material
 import org.bukkit.configuration.ConfigurationSection
+import org.bukkit.entity.Player
 import org.bukkit.inventory.ItemStack
 
 class Voucher private constructor(
     val id: String,
     val settings: VoucherSettings,
     val item: ItemStack,
-    val actions: List<ArcaneAction>
+    val actions: List<ArcaneAction>,
+    val bulkActions: List<ArcaneAction>
 ) {
+
+    fun redeem(player: Player, actionManager: ArcaneActionManager, isBulk: Boolean) {
+        actionManager.executeActions(player, actions)
+    }
 
     companion object {
 
@@ -22,16 +28,17 @@ class Voucher private constructor(
             val id = config.name
             val settings = VoucherSettings.from(config.getConfigurationSection("settings"))
             val item = itemCreator.create(true, config.getConfigurationSection("item"), Material.PAPER).apply {
-                val nbtItem = NBTItem(this)
-                val compound = nbtItem.getCompound(NBT.VOUCHER_COMPOUND)
+                val nbtItem = NBTItem(this, true)
+                val compound = nbtItem.getOrCreateCompound(NBT.VOUCHER_COMPOUND)
 
                 compound.addCompound(NBT.ARGUMENTS_COMPOUND)
                 compound.setString(NBT.VOUCHER_NAME, config.name)
                 nbtItem.item
             }
-            val actions = actionManager.parseActions(config.getStringList("actions"))
 
-            return Voucher(id, settings, item, actions)
+            val actions = actionManager.parseActions(config.getStringList("actions"))
+            val bulkActions = actionManager.parseActions(config.getStringList("bulkActions"))
+            return Voucher(id, settings, item, actions, bulkActions)
         }
 
     }
