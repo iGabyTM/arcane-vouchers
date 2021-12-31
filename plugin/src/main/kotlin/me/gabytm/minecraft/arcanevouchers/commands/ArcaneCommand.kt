7190316c79cdc2ad958d82492c03bc07e6961aa1
iGabyTM <contactgabytm@gmail.com>
2021-12-31 10:13:23 +0200

    [plugin] update ArcaneCommand - make it an abstract class - add methods to create completion for players and vouchers

--- plugin/src/main/kotlin/me/gabytm/minecraft/arcanevouchers/commands/ArcaneCommand.kt
+++ plugin/src/main/kotlin/me/gabytm/minecraft/arcanevouchers/commands/ArcaneCommand.kt
@@ -2,8 +2,33 @@ package me.gabytm.minecraft.arcanevouchers.commands
 
 import me.gabytm.minecraft.arcanevouchers.ArcaneVouchers
 import me.mattstudios.mf.base.CommandBase
+import org.bukkit.Bukkit
+import org.bukkit.util.StringUtil
 
-open class ArcaneCommand(plugin: ArcaneVouchers) : CommandBase(
+abstract class ArcaneCommand(protected val plugin: ArcaneVouchers) : CommandBase(
     plugin.config.getString("settings.command") ?: "arcanevouchers",
     plugin.config.getStringList("settings.alias")
-)
+) {
+
+    fun createPlayersCompletion(arg: String, includeAllOnlinePlayers: Boolean = false, includeAllPlayers: Boolean = false): MutableList<String> {
+        val collection = if (includeAllOnlinePlayers) mutableListOf(ALL_ONLINE_PLAYERS) else mutableListOf()
+
+        if (includeAllPlayers) {
+            collection.add(ALL_PLAYERS)
+        }
+
+        return StringUtil.copyPartialMatches(arg, Bukkit.getOnlinePlayers().map { it.name }, collection)
+    }
+
+    fun createVoucherCompletion(arg: String): MutableList<String> {
+        return StringUtil.copyPartialMatches(arg, plugin.voucherManager.getVoucherIds(), mutableListOf())
+    }
+
+    companion object {
+
+        const val ALL_ONLINE_PLAYERS = "*"
+        const val ALL_PLAYERS = "**"
+
+    }
+
+}
