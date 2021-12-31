d18e7221f3d5f842c8af9067659287024ea50de6
iGabyTM <contactgabytm@gmail.com>
2021-12-31 11:09:37 +0200

    [plugin] add extension for OfflinePlayer to get its name since `OfflinePlayer#getName` is nullable, with this method you can specify a default value in case it is null, e.g. `player.name(args[0])`

--- plugin/src/main/kotlin/me/gabytm/minecraft/arcanevouchers/functions/Players.kt
+++ plugin/src/main/kotlin/me/gabytm/minecraft/arcanevouchers/functions/Players.kt
@@ -1,9 +1,14 @@
 package me.gabytm.minecraft.arcanevouchers.functions
 
 import me.gabytm.minecraft.arcanevouchers.ServerVersion
+import org.bukkit.OfflinePlayer
 import org.bukkit.entity.Player
 import org.bukkit.inventory.ItemStack
 
+/**
+ * Get a [Player]'s item held using the right method for each game version
+ * @return the [ItemStack] that the player is holding
+ */
 @Suppress("DEPRECATION")
 fun Player.item(): ItemStack {
     return if (ServerVersion.HAS_OFF_HAND) {
@@ -12,3 +17,10 @@ fun Player.item(): ItemStack {
         this.inventory.itemInHand
     }
 }
+
+/**
+ * Get the name of an [OfflinePlayer] and return a default value if it is null
+ * @param default the default value returned in case the name is null
+ * @return [OfflinePlayer.name] or `default` if it is null
+ */
+fun OfflinePlayer.name(default: String = ""): String = name ?: default
