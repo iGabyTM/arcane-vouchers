6fe39d9a375d0d21e486056b20eabd38d60a71ad
iGabyTM <contactgabytm@gmail.com>
2022-01-21 13:33:21 +0200

    [plugin] add method to check if a Material is item on legacy versions

--- plugin/src/main/kotlin/me/gabytm/minecraft/arcanevouchers/utils/UtilsHandler.kt
+++ plugin/src/main/kotlin/me/gabytm/minecraft/arcanevouchers/utils/UtilsHandler.kt
@@ -15,6 +15,8 @@ import org.bukkit.inventory.ItemFlag
 import org.bukkit.potion.PotionEffectType
 import java.io.File
 import java.io.IOException
+import java.nio.file.Files
+import java.nio.file.StandardOpenOption
 import java.text.SimpleDateFormat
 import java.util.*
 
@@ -22,16 +24,10 @@ import java.util.*
 class UtilsHandler(plugin: ArcaneVouchers) {
 
     init {
-        val file = File(plugin.dataFolder, "UTILS.yml")
+        val file = File(plugin.dataFolder, ".UTILS.yml")
 
         if (file.exists()) {
             file.delete()
-        }
-
-        try {
-            file.createNewFile()
-        } catch (e: IOException) {
-            exception("Could not create $file", e)
         }
 
         val time = SimpleDateFormat("dd MMM yyyy, HH:mm z").format(Date())
@@ -70,15 +66,37 @@ class UtilsHandler(plugin: ArcaneVouchers) {
         }
     }
 
-    // TODO: 21-Jan-22 add method for legacy versions to check if the material is an item 
     private fun getMaterials(): List<String> {
         val filter: (Material) -> Boolean = if (ServerVersion.IS_LEGACY) {
-            { it != Material.AIR }
+            { it != Material.AIR && isItem(it) }
         } else {
             { it != Material.AIR && it.isItem && !it.isLegacy }
         }
 
         return Material.values().filter(filter).map { it.name }
+    }
+
+    private fun isItem(material: Material): Boolean {
+        return when (material.name) {
+            "ACACIA_DOOR", "BED_BLOCK", "BEETROOT_BLOCK",
+            "BIRCH_DOOR", "BREWING_STAND", "BURNING_FURNACE",
+            "CAKE_BLOCK", "CARROT", "CAULDRON", "COCOA",
+            "CROPS", "DARK_OAK_DOOR", "DAYLIGHT_DETECTOR_INVERTED",
+            "DIODE_BLOCK_OFF", "DIODE_BLOCK_ON", "DOUBLE_STEP",
+            "DOUBLE_STEP_SLAB2", "ENDER_PORTAL", "END_GATEWAY",
+            "FIRE", "FLOWER_POT", "FROSTED_ICE",
+            "GLOWING_REDSTONE_ORE", "IRON_DOOR_BLOCK", "JUNGLE_DOOR",
+            "LAVA", "MELON_STEM", "NETHER_WARDS",
+            "PISTON_EXTENSION", "PISTON_MOVING_PIECE", "PORTAL",
+            "POTATO", "PUMPKIN_STEM", "PURPUR_DOUBLE_STAB",
+            "REDSTONE_COMPARATOR_OFF", "REDSTONE_COMPARATOR_ON", "REDSTONE_LAMP_ON",
+            "REDSTONE_TORCH_OFF", "REDSTONE_WIRE", "SIGN_POST",
+            "SKULL", "SPRUCE_DOOR", "STANDING_BANNER",
+            "STATIONARY_LAVA", "STATIONARY_WATER", "SUGAR_CANE_BLOCK",
+            "TRIPWIRE", "WALL_BANNER", "WALL_SIGN",
+            "WATER", "WOODEN_DOOR", "WOOD_DOUBLE_STEP" -> false
+            else -> true
+        }
     }
 
     private fun getSounds(): List<String> {
