d154eea03157bcff7e43a597b8f31fc01c02f172
iGabyTM <contactgabytm@gmail.com>
2022-02-01 18:01:19 +0200

    [plugin] use the right bStats chart (AdvancedPie)

--- plugin/src/main/kotlin/me/gabytm/minecraft/arcanevouchers/BStats.kt
+++ plugin/src/main/kotlin/me/gabytm/minecraft/arcanevouchers/BStats.kt
@@ -1,7 +1,7 @@
 package me.gabytm.minecraft.arcanevouchers
 
 import org.bstats.bukkit.Metrics
-import org.bstats.charts.DrilldownPie
+import org.bstats.charts.AdvancedPie
 import org.bstats.charts.SingleLineChart
 
 class BStats(plugin: ArcaneVouchers) {
@@ -10,13 +10,12 @@ class BStats(plugin: ArcaneVouchers) {
     private val metrics = Metrics(plugin, 7199)
 
     init {
-        metrics.addCustomChart(DrilldownPie("actions") {
-            val actions = plugin.voucherManager.getVouchers()
+        metrics.addCustomChart(AdvancedPie("actions") {
+            plugin.voucherManager.getVouchers()
                 .flatMap { it.actions }
                 .map { it.getName() }
                 .groupingBy { it }
                 .eachCount()
-            mutableMapOf("actions" to actions)
         })
 
         metrics.addCustomChart(SingleLineChart("vouchers", plugin.voucherManager.getVoucherIds()::size))
