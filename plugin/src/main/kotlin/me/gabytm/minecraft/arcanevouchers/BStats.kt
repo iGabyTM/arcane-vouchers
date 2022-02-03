8ad6c3ce9add4391e05b03003e3781f64dddfc39
iGabyTM <contactgabytm@gmail.com>
2022-02-03 18:26:48 +0200

    [plugin] change the 'actions' bStats chart See how many servers use a certain action instead of in how many vouchers they are used

--- plugin/src/main/kotlin/me/gabytm/minecraft/arcanevouchers/BStats.kt
+++ plugin/src/main/kotlin/me/gabytm/minecraft/arcanevouchers/BStats.kt
@@ -14,6 +14,7 @@ class BStats(plugin: ArcaneVouchers) {
             plugin.voucherManager.getVouchers()
                 .flatMap { it.actions }
                 .map { it.getName() }
+                .distinct()
                 .groupingBy { it }
                 .eachCount()
         })
