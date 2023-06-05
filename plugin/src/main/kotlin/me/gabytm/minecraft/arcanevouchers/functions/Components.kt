af466b9873d6faf1463610fda4efc2a6d3ecddb4
iGabyTM <contactgabytm@gmail.com>
2023-06-05 19:32:08 +0300

    feat(plugin): parse placeholders in voucher name & lore (close #20)

--- plugin/src/main/kotlin/me/gabytm/minecraft/arcanevouchers/functions/Components.kt
+++ plugin/src/main/kotlin/me/gabytm/minecraft/arcanevouchers/functions/Components.kt
@@ -4,7 +4,7 @@ import net.kyori.adventure.text.Component
 import net.kyori.adventure.text.format.TextDecoration
 
 /**
- * Remove the [italic][TextDecoration.ITALIC] of a component, use for [item][org.bukkit.inventory.ItemStack] name and lore
+ * Remove the [italic][TextDecoration.ITALIC] of a component, used for [item][org.bukkit.inventory.ItemStack] name and lore
  * @return a [Component] without [italic][TextDecoration.ITALIC]
  */
 fun Component.removeItalic(): Component = decoration(TextDecoration.ITALIC, TextDecoration.State.FALSE)
